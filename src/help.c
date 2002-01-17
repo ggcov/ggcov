4ffd81aea79a347f8e60912a86f56fa7bd91e483
gnb <gnb>
2002-01-17 15:55:06 +0000

    x  Visual improvements to the About window.

--- src/help.c
+++ src/help.c
@@ -21,7 +21,7 @@
 #include "estring.h"
 #include "ui.h"
 
-CVSID("$Id: help.c,v 1.1 2002-01-17 03:52:47 gnb Exp $");
+CVSID("$Id: help.c,v 1.2 2002-01-17 04:55:06 gnb Exp $");
 
 static GtkWidget *about_window;
 static GtkWidget *licence_window;
@@ -67,11 +67,10 @@ on_about_licence_clicked(GtkWidget *w, gpointer data)
 }
 
 static const char warranty_str[] = N_("\
-GGCov comes with ABSOLUTELY NO\n\
-WARRANTY.  This is free software\n\
-and you are welcome to redistribute\n\
-it under certain conditions.  For details,\n\
-press the Licence button.\
+GGCov comes with ABSOLUTELY NO WARRANTY.\n\
+This is free software and you are welcome\n\
+to redistribute it under certain conditions.\n\
+For details, press the Licence button.\
 ");
 
 GLADE_CALLBACK void
@@ -93,7 +92,7 @@ on_about_activate(GtkWidget *w, gpointer data)
 	estring_init(&blurb);
 	estring_append_string(&blurb, blurb_proto);
 	estring_replace_all(&blurb, "@VERSION@", VERSION);
-	estring_replace_all(&blurb, "@AUTHOR@", "Greg Banks\n<gnb@alphalink.com.au>");
+	estring_replace_all(&blurb, "@AUTHOR@", "Greg Banks <gnb@alphalink.com.au>");
 	estring_replace_all(&blurb, "@WARRANTY@", _(warranty_str));
 	gtk_label_set_text(GTK_LABEL(about_label), blurb.data);
 	estring_free(&blurb);
