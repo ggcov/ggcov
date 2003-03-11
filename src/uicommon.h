d7aea8f7c2c7be12bfd9b2f3d603f2d5315fa994
gnb <gnb>
2003-03-11 17:28:26 +0000

    *   Partial gtk2 port derived from code contributed by Havoc Pennington.

---
+++ src/uicommon.h
@@ -0,0 +1,42 @@
+/*
+ * ggcov - A GTK frontend for exploring gcov coverage data
+ * Copyright (c) 2001 Greg Banks <gnb@alphalink.com.au>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _ggcov_ui_common_h_
+#define _ggcov_ui_common_h_ 1
+
+#include <gtk/gtk.h>
+
+
+#if GTK_CHECK_VERSION(1,99,0)
+#define GTK2 1
+#else
+#define GTK2 0
+#endif
+
+/* TODO: do this properly in configure.in */
+#if GTK2
+#define HAVE_LIBGNOME	0
+#define HAVE_LIBGNOMEUI	0
+#else
+#define HAVE_LIBGNOME	1
+#define HAVE_LIBGNOMEUI	2
+#endif
+
+
+#endif /* _ggcov_ui_common_h_ */
