b8f23c06ed8c7c4e9670e82eb20af75c4a376ff1
gnb <gnb>
2003-05-11 10:30:09 +0000

    *   Added string_var and tok_t classes for string manipulation.

---
+++ src/tok.C
@@ -0,0 +1,58 @@
+/*
+ * CANT - A C implementation of the Apache/Tomcat ANT build system
+ * Copyright (c) 2001 Greg Banks <gnb@alphalink.com.au>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "tok.H"
+
+CVSID("$Id: tok.C,v 1.1 2003-05-11 00:30:22 gnb Exp $");
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+void
+tok_t::init(char *str, const char *sep)
+{
+    buf_ = str;
+    state_ = 0;
+    sep_ = (sep == 0 ? " \t\r\n" : sep);
+}
+
+tok_t::tok_t(const char *str, const char *sep)
+{
+    init(g_strdup(str), sep);
+}
+
+tok_t::tok_t(char *str, const char *sep)
+{
+    init(str, sep);
+}
+
+tok_t::~tok_t()
+{
+    g_free(buf_);
+}
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+const char *
+tok_t::next()
+{
+    return strtok_r((state_ == 0 ? buf_ : 0), sep_, &state_);
+}
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+/*END*/
