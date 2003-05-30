6f8bcdea2da5e0b54cac94565b409576aa42bec3
gnb <gnb>
2003-05-30 00:53:57 +0000

    *  Added shell script to run ggcov under valgrind.

---
+++ valgrind-run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# ggcov - A GTK frontend for exploring gcov coverage data
+# Copyright (c) 2001-2003 Greg Banks <gnb@alphalink.com.au>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+valgrind \
+    --num-callers=16 \
+    --leak-check=yes \
+    --workaround-gcc296-bugs=yes \
+    src/ggcov "$@" \
+    2>&1 | tee vg$$.log
