00a383db3346c1a20ca763781797b4c5c4611366
gnb <gnb>
2004-11-20 19:51:30 +0000

    *  portability fixes to enable platform.sh to work on Solaris *  reordered platform string to put version last

--- test/platform.sh
+++ test/platform.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
-NAME=`uname -s | tr 'A-Z' 'a-z'`
-ARCH=`uname -m | tr 'A-Z' 'a-z'`
-REV=`uname -r | tr 'A-Z' 'a-z'`
+tolower1 ()
+{
+    tr '[:upper:]' '[:lower:]'
+}
+
+tolower2 ()
+{
+    tr 'A-Z' 'a-z'
+}
+
+X=`echo FOo | tolower1`
+if [ "$X" = "foo" ]; then
+    tolower=tolower1
+else
+    X=`echo FOo | tolower2`
+    if [ "$X" = "foo" ]; then
+	tolower=tolower2
+    else
+	echo "platform.sh: no functional tr program" 1>&2
+	exit 1
+    fi
+fi
+
+NAME=`uname -s | $tolower`
+ARCH=`uname -m | $tolower`
+REV=`uname -r | $tolower`
 VENDOR=unknown
 
 get_linux_distro ()
@@ -29,14 +52,24 @@ linux)
     REV=$2
     ;;
 irix64) NAME=irix ;;
+sunos)
+    case "$REV" in
+    5.*)
+	NAME=solaris
+	REV=`echo $REV | sed -e s'/^5\.//'`
+	;;
+    esac
+    VENDOR=sun
+    ;;
 esac
 
 case "$ARCH" in
 i[3-9]86) ARCH=i386 ;;
 IP[1-9]|IP[1-9][0-9]) ARCH=mips ;;
+sun4[cmu]) ARCH=sparc ;;
 esac
 
 REV=`echo "$REV" | sed -e 's|-.*||' -e 's|\.||g'`
 
-echo ${ARCH}-${VENDOR}-${REV}-${NAME}
+echo ${ARCH}-${VENDOR}-${NAME}-${REV}
 
