ec43e625a797e3cefb0fc1b0a008d11c3da355fc
gnb <gnb>
2005-05-18 23:52:55 +0000

    * Wrap the bar graph generating code in a page class.

--- php/lib/covbar.php
+++ php/lib/covbar.php
@@ -17,54 +17,99 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 // 
-// $Id: covbar.php,v 1.1 2005-05-18 13:14:18 gnb Exp $
+// $Id: covbar.php,v 1.2 2005-05-18 13:52:55 gnb Exp $
 //
 
-require_once 'cov.php';
+require_once 'ggcov/lib/cov.php';
 
-function get_number($name, $def)
+class cov_covbar_page extends cov_page
 {
-    if (array_key_exists($name, $_GET))
-	$x = (int)$_GET[$name];
-    else
-	$x = $def;
-    return $x;
+    var $env_;
+    var $width_;
+    var $height_;
+    var $values_;
+    var $format_;
+
+    function cov_covbar_page($e)
+    {
+	$this->env_ = $e;
+    }
+
+    function get_number($get, $name, $def)
+    {
+	if (array_key_exists($name, $get))
+	    $x = (int)$get[$name];
+	else
+	    $x = $def;
+	return $x;
+    }
+
+    function parse_args($get)
+    {
+	$cb = $this->env_->cb_;
+
+	$this->width_ = $this->get_number($get, 'w', 150);
+	$this->height_ = $this->get_number($get, 'h', 25);
+
+	$this->values_ = array();
+	$this->values_[COV_COVERED] = $this->get_number($get, 'c', 0);
+	$this->values_[COV_PARTCOVERED] = $this->get_number($get, 'pc', 0);
+	$this->values_[COV_UNCOVERED] = $this->get_number($get, 'uc', 0);
+
+	if (function_exists('imagegif'))
+	    $this->format_ = 'gif';
+	else if (function_exists('imagepng'))
+	    $this->format_ = 'png';
+	else
+	    $cb->fatal("Can't find any image format output function");
+    }
+
+    function content_type()
+    {
+	return 'image/gif';
+    }
+
+    function title()
+    {
+	return null;
+    }
+
+    function render()
+    {
+	global $cov_rgb;
+	$cb = $this->env_->cb_;
+
+	$total = array_sum($this->values_);
+	if ($total == 0)
+	    exit;   // die and emit no output
+	else if ($total < 0)
+	    $cb->fatal("illegal count total");
+
+	$img = imagecreate($this->width_, $this->height_);
+
+	$x = 0;
+	foreach (array(COV_COVERED, COV_PARTCOVERED, COV_UNCOVERED) as $status)
+	{
+	    $rgb = $cov_rgb[$status];
+	    $col = imagecolorallocate($img, $rgb[0], $rgb[1], $rgb[2]);
+	    $w = $this->width_ * $this->values_[$status] / $total;
+	    imagefilledrectangle($img, $x, 0, $x+$w, $this->height_, $col);
+	    $x += $w;
+	}
+
+	header("Content-type: image/$this->format_");
+	switch ($this->format_)
+	{
+	case 'gif':
+	    imagegif($img);
+	    break;
+	case 'png':
+	    imagepng($img);
+	    break;
+	}
+
+	imagedestroy($img);
+    }
 }
-
-$width = get_number('w', 150);
-$height = get_number('h', 25);
-
-$values = array();
-$values[COV_COVERED] = get_number('c', 0);
-$values[COV_PARTCOVERED] = get_number('pc', 0);
-$values[COV_UNCOVERED] = get_number('uc', 0);
-$total = array_sum($values);
-if ($total <= 0)
-    return;
-
-$img = imagecreate($width, $height);
-
-$x = 0;
-foreach (array(COV_COVERED, COV_PARTCOVERED, COV_UNCOVERED) as $status)
-{
-    $rgb = $cov_rgb[$status];
-    $col = imagecolorallocate($img, $rgb[0], $rgb[1], $rgb[2]);
-    $w = $width * $values[$status] / $total;
-    imagefilledrectangle($img, $x, 0, $x+$w, $height, $col);
-    $x += $w;
-}
-
-if (function_exists('imagegif'))
-{
-    header('Content-type: image/gif');
-    imagegif($img);
-}
-else if (function_exists('imagepng'))
-{
-    header('Content-type: image/png');
-    imagepng($img);
-}
-
-imagedestroy($img);
 
 ?>
