3028ddd83f8e0d812fe88167fcd3474f36de7033
Greg Banks <gnb@users.sourceforge.net>
2011-03-01 21:58:44 +1100

    Add a report which emits a Cobertura coverage.xml
    Still some rough edges.
    
    - the "complexity" measure is fixed at 0.0.  I have no idea
      how this is supposed to be measured.
    
    - the mapping between Java concepts like package and the nearest
      equivalents in C is pretty loose.
    
    This requires tggcov to use libxml2, which the GNOME interface did
    before because libglade uses it.

--- src/report.C
+++ src/report.C
@@ -1,6 +1,6 @@
 /*
  * ggcov - A GTK frontend for exploring gcov coverage data
- * Copyright (c) 2004 Greg Banks <gnb@users.sourceforge.net>
+ * Copyright (c) 2001-2011 Greg Banks <gnb@users.sourceforge.net>
  * 
  *
  * TODO: attribution for decode-gcov.c
@@ -21,10 +21,13 @@
  */
 
 #include "common.h"
+#include <sys/time.h>
+#include <libxml/tree.h>
 #include "cov.H"
 #include "filename.h"
 #include "estring.H"
 #include "report.H"
+#include "tok.H"
 
 CVSID("$Id: report.C,v 1.4 2010-05-09 05:37:15 gnb Exp $");
 
@@ -283,6 +286,303 @@ report_incompletely_covered_functions_per_file(FILE *fp)
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 
+class xml_node_t : private xmlNode
+{
+public:
+    static xml_node_t *wrap(xmlNode *n) { return (xml_node_t *)n; }
+    xmlNode *unwrap() { return (xmlNode *)this; }
+    xml_node_t *new_child(const char *name, const char *content = 0)
+    {
+	return (xml_node_t *)xmlNewChild((xmlNode *)this,
+					 0,
+					 (const xmlChar *)name,
+					 (const xmlChar *)content);
+    }
+    void add_prop(const char *name, const char *value)
+    {
+	xmlNewProp((xmlNode *)this, (const xmlChar *)name, (const xmlChar *)value);
+    }
+    void add_propf(const char *name, const char *fmt, ...)
+	__attribute__((format(printf,3,4)))
+    {
+	estring e;
+	va_list args;
+	va_start(args, fmt);
+	e.append_vprintf(fmt, args);
+	va_end(args);
+	xmlNewProp((xmlNode *)this, (const xmlChar *)name, (const xmlChar *)e.data());
+    }
+};
+
+
+class cob_report_t
+{
+public:
+    cob_report_t();
+    ~cob_report_t();
+
+    void add(cov_file_t *);
+    void post_add();
+    void emit(FILE *fp);
+
+    struct package_t
+    {
+	string_var name_;
+	list_t<cov_file_t> files_;
+	cov_stats_t stats_;
+
+	package_t(char *name)
+	 :  name_(name)
+	{
+	}
+
+	~package_t()
+	{
+	    files_.remove_all();
+	}
+    };
+private:
+
+    char *path(cov_file_t *f);
+    void post_add_lines(xml_node_t *xparent, cov_file_t *f,
+		        unsigned int first, unsigned int last);
+    void setup_common();
+    void setup_xdoc();
+    void post_add_coverage_props(xml_node_t *, const cov_stats_t *, int);
+    void post_add_method(xml_node_t *, cov_function_t *fn);
+    void post_add_class(xml_node_t *, package_t *pkg, cov_file_t *f);
+    void post_add_package(xml_node_t *, package_t *pkg);
+
+    hashtable_t<const char, package_t> *packages_;
+    const char *common_;
+    unsigned int common_len_;
+    cov_stats_t stats_;
+    xmlDoc *xdoc_;
+    xml_node_t *xroot_;
+};
+
+cob_report_t::cob_report_t()
+{
+    packages_ = new hashtable_t<const char, package_t>;
+
+    setup_common();
+    setup_xdoc();
+}
+
+void
+cob_report_t::setup_common()
+{
+    common_ = cov_file_t::common_path();
+    common_len_ = strlen(common_);
+
+    /* double check that common_ starts and ends with a / and is at
+     * least 3 chars long -- we rely on this */
+    assert(common_ != 0);
+    assert(common_len_ >= 3);
+    assert(common_[0] == '/');
+    assert(common_[common_len_-1] == '/');
+
+    const char *p = common_ + common_len_ - 2;
+    while (p >= common_ && *p != '/')
+	--p;
+    common_len_ = p+1 - common_;
+}
+
+void
+cob_report_t::setup_xdoc()
+{
+    xdoc_ = xmlNewDoc((const xmlChar *)XML_DEFAULT_VERSION);
+    xmlCreateIntSubset(xdoc_,
+		       (const xmlChar *)"coverage",
+		       (const xmlChar *)"coverage",
+		       (const xmlChar *)"http://cobertura.sourceforge.net/xml/coverage-04.dtd");
+    xroot_ = xml_node_t::wrap(xmlNewDocNode(xdoc_, 0,
+			      (const xmlChar *)"coverage", 0));
+    xmlDocSetRootElement(xdoc_, xroot_->unwrap());
+
+    string_var common = g_strndup(common_, common_len_);
+    xroot_->new_child("sources")->new_child("source", common);
+
+    /* Fake a Java timestamp which appears to be milliseconds
+     * since the Unix epoch */
+    struct timeval now;
+    char buf[64];
+    gettimeofday(&now, 0);
+    snprintf(buf, sizeof(buf), "%lu%03u",
+	    (unsigned long)now.tv_sec,
+	    ((unsigned)now.tv_usec) / 1000);
+    xroot_->add_prop("timestamp", buf);
+    xroot_->add_prop("version", "1.9");
+}
+
+cob_report_t::~cob_report_t()
+{
+    xmlFreeDoc(xdoc_);
+    delete packages_;
+}
+
+//
+// Make and return a new string which is a partial pathname for the
+// given file but starting with a common first directory rather than
+// being minimal.  This ensures that the cobertura file sees a root
+// <package> instead of trying to put <class>es in <coverage>.
+//
+char *
+cob_report_t::path(cov_file_t *f)
+{
+    return g_strconcat(common_ + common_len_, f->minimal_name(), (char *)0);
+}
+
+void
+cob_report_t::add(cov_file_t *f)
+{
+    string_var fpath = path(f);
+    string_var ppath = g_dirname(fpath);
+    package_t *pkg = packages_->lookup(ppath);
+    if (!pkg)
+    {
+	pkg = new package_t(ppath.take());
+	packages_->insert(pkg->name_, pkg);
+    }
+    pkg->files_.append(f);
+}
+
+void
+cob_report_t::post_add_lines(
+    xml_node_t *xparent,
+    cov_file_t *f,
+    unsigned int first,
+    unsigned int last)
+{
+    xml_node_t *xlines = xparent->new_child("lines");
+    unsigned int lineno;
+    for (lineno = first ; lineno <= last ; lineno++)
+    {
+	cov_line_t *ln = f->nth_line(lineno);
+	if (ln->status() == cov::UNINSTRUMENTED ||
+	    ln->status() == cov::SUPPRESSED)
+	    continue;
+	xml_node_t *xline = xlines->new_child("line");
+	xline->add_propf("number", "%u", lineno);
+	xline->add_propf("hits", "%llu", ln->count());
+	xline->add_prop("branch", "false"); /* TODO */
+    }
+}
+
+void
+cob_report_t::post_add_coverage_props(
+    xml_node_t *xnode,
+    const cov_stats_t *stats,
+    int level)
+{
+    xnode->add_propf("line-rate", "%f", stats->lines_fraction());
+    xnode->add_propf("branch-rate", "%f", stats->branches_fraction());
+    if (!level--)
+	return;
+    xnode->add_prop("complexity", "0.0"); /* TODO: WTF is this? */
+    if (!level--)
+	return;
+    xnode->add_propf("lines-covered", "%lu", stats->lines_executed());
+    xnode->add_propf("lines-valid", "%lu", stats->lines_total());
+    xnode->add_propf("branches-covered", "%lu", stats->branches_executed());
+    xnode->add_propf("branches-valid", "%lu", stats->branches_total());
+}
+
+void
+cob_report_t::post_add_method(xml_node_t *xmethods, cov_function_t *fn)
+{
+    cov_function_scope_t scope(fn);
+    if (scope.status() == cov::SUPPRESSED)
+	return;
+    xml_node_t *xmethod = xmethods->new_child("method");
+    xmethod->add_prop("name", fn->name());
+    /* TODO: do proper demangling of C++ names */
+    xmethod->add_propf("signature", "void %s(void)", fn->name());
+    post_add_coverage_props(xmethod, scope.get_stats(), 0);
+
+    const cov_location_t *first = fn->get_first_location();
+    const cov_location_t *last = fn->get_last_location();
+    if (!strcmp(first->filename, last->filename) &&
+	!strcmp(first->filename, fn->file()->name()))
+	post_add_lines(xmethod, fn->file(), first->lineno, last->lineno);
+}
+
+void
+cob_report_t::post_add_class(
+    xml_node_t *xclasses,
+    package_t *pkg,
+    cov_file_t *f)
+{
+    xml_node_t *xclass = xclasses->new_child("class");
+
+    string_var fpath = path(f);
+    xclass->add_prop("name", fpath);
+    xclass->add_prop("filename", fpath);
+
+    cov_file_scope_t scope(f);
+    const cov_stats_t *stats = scope.get_stats();
+    pkg->stats_.accumulate(stats);
+    post_add_coverage_props(xclass, stats, 1);
+
+    xml_node_t *xmethods = xclass->new_child("methods");
+    unsigned int i;
+    for (i = 0 ; i < f->num_functions() ; i++)
+	post_add_method(xmethods, f->nth_function(i));
+
+    post_add_lines(xclass, f, 1, f->num_lines());
+}
+
+void
+cob_report_t::post_add_package(xml_node_t *xpackages, package_t *pkg)
+{
+    xml_node_t *xpkg = xpackages->new_child("package");
+    xpkg->add_prop("name", pkg->name_);
+    xml_node_t *xclasses = xpkg->new_child("classes");
+
+    list_iterator_t<cov_file_t> fiter;
+    for (fiter = pkg->files_.first() ; fiter != (cov_file_t *)0 ; ++fiter)
+	post_add_class(xclasses, pkg, *fiter);
+
+    post_add_coverage_props(xpkg, &pkg->stats_, 1);
+    stats_.accumulate(&pkg->stats_);
+}
+
+void
+cob_report_t::post_add()
+{
+    xml_node_t *xpackages = xroot_->new_child("packages");
+    hashtable_iter_t<const char, package_t> hiter;
+    for (hiter = packages_->first() ; *hiter ; ++hiter)
+    {
+	hiter.remove();
+	post_add_package(xpackages, *hiter);
+    }
+    post_add_coverage_props(xroot_, &stats_, 2);
+}
+
+void
+cob_report_t::emit(FILE *fp)
+{
+    xmlDocDump(fp, xdoc_);
+}
+
+static int
+report_cobertura(FILE *fp)
+{
+    cob_report_t report;
+
+    list_iterator_t<cov_file_t> fiter;
+    for (fiter = cov_file_t::first() ; fiter != (cov_file_t *)0 ; ++fiter)
+	report.add(*fiter);
+
+    report.post_add();
+    report.emit(fp);
+
+    return 1;
+}
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
 const report_t all_reports[] = 
 {
 #define report(name, label) { #name, label, report_##name },
@@ -296,6 +596,8 @@ const report_t all_reports[] =
     	   N_("Poorly covered functions per file"))
     report(incompletely_covered_functions_per_file,
     	   N_("Incompletely covered functions per file"))
+    report(cobertura,
+    	   N_("Cobertura-compatible XML report"))
 #undef report
     {0, 0}
 };
