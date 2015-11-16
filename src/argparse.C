de19e62054c501e4191608e5cbae9951aeef20a1
Greg Banks <gnb@fastmail.fm>
2015-11-15 16:22:36 -0800

    Add a new C++-friendly argument parsing module
    whose design is loosely based on the commonly used Python and Java
    argument parsing libraries.

---
+++ src/argparse.C
@@ -0,0 +1,275 @@
+#include "argparse.H"
+
+namespace argparse
+{
+
+params_t::params_t()
+ :  files_(new ptrarray_t<const char>())
+{
+}
+
+params_t::~params_t()
+{
+    delete files_;
+}
+
+bool
+params_t::set(const char *key, const char *value)
+{
+    return false;
+}
+
+void
+params_t::add_file(const char *file)
+{
+    files_->append(file);
+}
+
+unsigned int
+params_t::num_files() const
+{
+    return files_->length();
+}
+
+const char *
+params_t::nth_file(unsigned i) const
+{
+    return files_->nth(i);
+}
+
+params_t::file_iterator_t
+params_t::file_iter() const
+{
+    return files_->first();
+}
+
+void
+params_t::setup_parser(parser_t &)
+{
+}
+
+void
+params_t::post_args()
+{
+}
+
+simple_params_t::simple_params_t()
+ :  values_(new hashtable_t<const char, const char>)
+{
+}
+
+simple_params_t::~simple_params_t()
+{
+    delete values_;
+}
+
+bool
+simple_params_t::set(const char *key, const char *value)
+{
+    if (!value)
+	value = "1";
+    values_->insert(key, value);
+    return true;
+}
+
+bool
+simple_params_t::has(const char *key) const
+{
+    return values_->lookup_extended(key, NULL, NULL);
+}
+
+const char *
+simple_params_t::value(const char *key) const
+{
+    return values_->lookup(key);
+}
+
+void
+simple_params_t::post_args()
+{
+}
+
+parser_t::parser_t(params_t &params)
+ :  params_(params),
+    options_(new ptrarray_t<option_t>),
+    popt_options_(0)
+{
+    params_.setup_parser(*this);
+}
+
+parser_t::~parser_t()
+{
+    for (ptrarray_iterator_t<option_t> itr = options_->first() ; *itr ; ++itr)
+	delete *itr;
+    delete options_;
+    delete popt_options_;
+}
+
+option_t::option_t(char short_option, const char *long_option)
+ :  name_(long_option),
+    short_option_(short_option),
+    long_option_(long_option),
+    flags_(0),		// generic setter, no arguments
+    setter_(0)
+{
+    if (!name_.data())
+    {
+	char buf[2];
+	buf[0] = short_option;
+	buf[1] = '\0';
+	name_ = (const char *)buf;
+    }
+}
+
+option_t::~option_t()
+{
+}
+
+bool
+option_t::set(params_t &params, const char *arg) const
+{
+    if ((flags_ & F_SETTER))
+    {
+	if ((flags_ & F_ARG))
+	{
+	    (params.*setter_)(arg);
+	}
+	else
+	{
+	    void (params_t::*setter)() = (void (params_t::*)())setter_;
+	    (params.*setter)();
+	}
+	return true;
+    }
+    else
+    {
+	if (!(flags_ & F_ARG))
+	    arg = 0;	// make sure this has an actual non-random value
+	return params.set(name_.data(), arg);
+    }
+}
+
+void
+option_t::build_popt_option(struct poptOption *po)
+{
+    memset(po, 0, sizeof(*po));
+    po->longName = long_option_.data();
+    po->shortName = short_option_;
+    po->argInfo = ((flags_ & F_ARG) ? POPT_ARG_STRING : POPT_ARG_NONE);
+    po->descrip = description_.data();
+}
+
+option_t &
+parser_t::add_option(char short_name, const char *long_name)
+{
+    option_t *o = new option_t(short_name, long_name);
+    options_->append(o);
+    return *o;
+}
+
+void
+parser_t::popt_callback(poptContext con,
+			enum poptCallbackReason reason,
+			const struct poptOption *opt,
+			const char *arg,
+			const void *data)
+{
+    if (reason == POPT_CALLBACK_REASON_OPTION)
+    {
+	parser_t *self = (parser_t *)data;
+	self->options_->nth(opt->val)->set(self->params_, arg);
+    }
+}
+
+struct poptOption *
+parser_t::get_popt_table()
+{
+    if (popt_options_ == 0)
+    {
+	static const struct poptOption headers[] =
+	{
+	    /* This entry hooks up the popt_callback to be called
+	     * whenever the popt library detects an option.  A fact
+	     * not documented in the manpage is that this entry
+	     * must be the first in the table.
+	     */
+	    {
+		0,				/* longname */
+		0,                              /* shortname */
+		POPT_ARG_CALLBACK,              /* argInfo */
+		(void *)popt_callback,		/* arg */
+		0,                              /* val 0=don't return */
+		/*filled in later*/0,		/* descrip */
+		0                               /* argDescrip */
+	    }
+	};
+	static const unsigned int nheaders = sizeof(headers)/sizeof(headers[0]);
+	static const struct poptOption trailers[] =
+	{
+	    /* This entry adds --help implemented internally in libpopt */
+	    POPT_AUTOHELP
+	    /* This entry marks the end of the options table */
+	    POPT_TABLEEND
+	};
+	static const unsigned int ntrailers = sizeof(trailers)/sizeof(trailers[0]);
+
+	struct poptOption *popts = new struct poptOption[nheaders + options_->length() + ntrailers];
+
+	memcpy(&popts[0], headers, sizeof(headers));
+	// this is for POPT_ARG_CALLBACK
+	popts[0].descrip = (const char *)this;
+
+	int n = nheaders;
+	for (ptrarray_iterator_t<option_t> itr = options_->first() ; *itr ; ++itr)
+	{
+	    (*itr)->build_popt_option(&popts[n]);
+	    popts[n].val = (n-nheaders);
+	    n++;
+	}
+
+	memcpy(&popts[n], trailers, sizeof(trailers));
+
+	popt_options_ = popts;
+    }
+
+    return popt_options_;
+}
+
+int
+parser_t::parse(int argc, char **argv)
+{
+    poptContext popt_context = poptGetContext(PACKAGE, argc, (const char**)argv, get_popt_table(), 0);
+    if (other_option_help_.data())
+	poptSetOtherOptionHelp(popt_context, other_option_help_.data());
+
+    int rc;
+    while ((rc = poptGetNextOpt(popt_context)) > 0)
+	;
+    if (rc < -1)
+    {
+	fprintf(stderr, "%s:%s at or near %s\n",
+	    argv[0],
+	    poptStrerror(rc),
+	    poptBadOption(popt_context, POPT_BADOPTION_NOALIAS));
+	poptFreeContext(popt_context);
+	return -1;
+    }
+
+    handle_popt_tail(popt_context);
+
+    poptFreeContext(popt_context);
+
+    return 0;
+}
+
+void
+parser_t::handle_popt_tail(poptContext popt_context)
+{
+    const char *file;
+    while ((file = poptGetArg(popt_context)) != 0)
+	params_.add_file(file);
+    params_.post_args();
+}
+
+// close the namespace
+}
---
+++ src/argstest.C
@@ -0,0 +1,246 @@
+#include "argparse.H"
+#include "testfw.h"
+
+TEST(simple)
+{
+#define TESTCASE(...) \
+    argparse::simple_params_t params; \
+    argparse::parser_t parser(params); \
+    parser.add_option('s', "seitan").without_arg(); \
+    parser.add_option('b', "brooklyn").with_arg(); \
+    static const char * const _argv[] = { __VA_ARGS__, NULL }; \
+    dmsg("line %d", __LINE__); \
+    int r = parser.parse(sizeof(_argv)/sizeof(_argv[0])-1, (char **)_argv)
+    {
+	TESTCASE("/bin/the-program");
+	check_num_equals(r, 0);
+	check(!params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "mustache");
+	check_num_equals(r, 0);
+	check(!params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 1);
+	check_str_equals(params.nth_file(0), "mustache");
+    }
+    {
+	TESTCASE("/bin/the-program", "mustache", "dreamcatcher");
+	check_num_equals(r, 0);
+	check(!params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 2);
+	check_str_equals(params.nth_file(0), "mustache");
+	check_str_equals(params.nth_file(1), "dreamcatcher");
+    }
+    {
+	TESTCASE("/bin/the-program", "-b", "shoreditch");
+	check_num_equals(r, 0);
+	check(!params.has("seitan"));
+	check(params.has("brooklyn"));
+	check_str_equals(params.value("brooklyn"), "shoreditch");
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "--brooklyn", "williamsburg");
+	check_num_equals(r, 0);
+	check(!params.has("seitan"));
+	check(params.has("brooklyn"));
+	check_str_equals(params.value("brooklyn"), "williamsburg");
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "-s");
+	check_num_equals(r, 0);
+	check(params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "--seitan");
+	check_num_equals(r, 0);
+	check(params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "-b", "leggings", "-s");
+	check_num_equals(r, 0);
+	check(params.has("seitan"));
+	check(params.has("brooklyn"));
+	check_str_equals(params.value("brooklyn"), "leggings");
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "-b", "leggings", "-s", "freegan");
+	check_num_equals(r, 0);
+	check(params.has("seitan"));
+	check(params.has("brooklyn"));
+	check_str_equals(params.value("brooklyn"), "leggings");
+	check_num_equals(params.num_files(), 1);
+	check_str_equals(params.nth_file(0), "freegan");
+    }
+    {
+	/* an unexpected option */
+	TESTCASE("/bin/the-program", "--bogus-option");
+	check_num_equals(r, -1);
+	check(!params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	/* an option which requires an argument but doesn't have one */
+	TESTCASE("/bin/the-program", "-b");
+	check_num_equals(r, -1);
+	check(!params.has("seitan"));
+	check(!params.has("brooklyn"));
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	/* the -- option turns all following arguments into files */
+	TESTCASE("/bin/the-program", "-b", "kombucha", "--", "biodiesel", "-s", "mustache");
+	check_num_equals(r, 0);
+	check(!params.has("seitan"));
+	check(params.has("brooklyn"));
+	check_str_equals(params.value("brooklyn"), "kombucha");
+	check_num_equals(params.num_files(), 3);
+	check_str_equals(params.nth_file(0), "biodiesel");
+	check_str_equals(params.nth_file(1), "-s");   /* -- makes this a file not an option */
+	check_str_equals(params.nth_file(2), "mustache");
+    }
+#undef TESTCASE
+}
+
+class argstest_params_t : public argparse::params_t
+{
+public:
+    argstest_params_t();
+    ~argstest_params_t();
+
+    ARGPARSE_STRING_PROPERTY(brooklyn);
+    ARGPARSE_BOOL_PROPERTY(seitan);
+
+    void setup_parser(argparse::parser_t &parser)
+    {
+	parser.add_option('s', "seitan")
+	      .setter((argparse::noarg_setter_t)&argstest_params_t::set_seitan);
+	parser.add_option('b', "brooklyn")
+	      .setter((argparse::arg_setter_t)&argstest_params_t::set_brooklyn);
+    }
+};
+
+argstest_params_t::argstest_params_t()
+ :  seitan_(false)
+{
+}
+
+argstest_params_t::~argstest_params_t()
+{
+}
+
+TEST(setter)
+{
+#define TESTCASE(...) \
+    argstest_params_t params; \
+    argparse::parser_t parser(params); \
+    static const char * const _argv[] = { __VA_ARGS__, NULL }; \
+    dmsg("line %d", __LINE__); \
+    int r = parser.parse(sizeof(_argv)/sizeof(_argv[0])-1, (char **)_argv)
+    {
+	TESTCASE("/bin/the-program");
+	check_num_equals(r, 0);
+	check(!params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "mustache");
+	check_num_equals(r, 0);
+	check(!params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 1);
+	check_str_equals(params.nth_file(0), "mustache");
+    }
+    {
+	TESTCASE("/bin/the-program", "mustache", "dreamcatcher");
+	check_num_equals(r, 0);
+	check(!params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 2);
+	check_str_equals(params.nth_file(0), "mustache");
+	check_str_equals(params.nth_file(1), "dreamcatcher");
+    }
+    {
+	TESTCASE("/bin/the-program", "-b", "shoreditch");
+	check_num_equals(r, 0);
+	check(!params.get_seitan());
+	check_str_equals(params.get_brooklyn(), "shoreditch");
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "--brooklyn", "williamsburg");
+	check_num_equals(r, 0);
+	check(!params.get_seitan());
+	check_str_equals(params.get_brooklyn(), "williamsburg");
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "-s");
+	check_num_equals(r, 0);
+	check(params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "--seitan");
+	check_num_equals(r, 0);
+	check(params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "-b", "leggings", "-s");
+	check_num_equals(r, 0);
+	check(params.get_seitan());
+	check_str_equals(params.get_brooklyn(), "leggings");
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	TESTCASE("/bin/the-program", "-b", "leggings", "-s", "freegan");
+	check_num_equals(r, 0);
+	check(params.get_seitan());
+	check_str_equals(params.get_brooklyn(), "leggings");
+	check_num_equals(params.num_files(), 1);
+	check_str_equals(params.nth_file(0), "freegan");
+    }
+    {
+	/* an unexpected option */
+	TESTCASE("/bin/the-program", "--bogus-option");
+	check_num_equals(r, -1);
+	check(!params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	/* an option which requires an argument but doesn't have one */
+	TESTCASE("/bin/the-program", "-b");
+	check_num_equals(r, -1);
+	check(!params.get_seitan());
+	check_null(params.get_brooklyn());
+	check_num_equals(params.num_files(), 0);
+    }
+    {
+	/* the -- option turns all following arguments into files */
+	TESTCASE("/bin/the-program", "-b", "kombucha", "--", "biodiesel", "-s", "mustache");
+	check_num_equals(r, 0);
+	check(!params.get_seitan());
+	check_str_equals(params.get_brooklyn(), "kombucha");
+	check_num_equals(params.num_files(), 3);
+	check_str_equals(params.nth_file(0), "biodiesel");
+	check_str_equals(params.nth_file(1), "-s");   /* -- makes this a file not an option */
+	check_str_equals(params.nth_file(2), "mustache");
+    }
+#undef TESTCASE
+}
