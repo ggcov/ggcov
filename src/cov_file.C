e9e8bed052a0102b0b3527ddcb4537cf71aecc3a
Alastair McKinstry <mckinstry@debian.org>
2016-01-11 07:38:44 +1100

    GCC 5.* Support

--- src/cov_file.C
+++ src/cov_file.C
@@ -977,6 +977,10 @@ cov_file_t::read_gcc3_bbg_file(covio_t *io,
 	if (expect_version != BBG_VERSION_GCC33)
 	    bbg_failed1("unexpected version=0x%08x", format_version_);
 	break;
+    case _NEW_VERSION(5,3,'*'):
+    case _NEW_VERSION(5,2,'*'):
+    case _NEW_VERSION(5,1,'*'):
+    case _NEW_VERSION(5,0,'*'):
     case _NEW_VERSION(4,9,'*'):
     case _NEW_VERSION(4,8,'R'): /* Fedora 19 */
     case _NEW_VERSION(4,8,'*'):
