7d13c669b81a9dce57b11c6fb80c7c4c974fae62
Greg Banks <gnb@fastmail.fm>
2019-03-09 20:46:54 -0800

    Fix Fedora provisioning script to ensure we have Ruby mustache

--- build/vagrant/fedora.sh
+++ build/vagrant/fedora.sh
@@ -6,5 +6,5 @@ yum -y install \
     git valgrind-devel binutils-devel libxml2-devel \
     doxygen python-pygments python-markdown \
     perl-XML-LibXML strace python-pip \
-    glib2-devel gd-devel popt-devel
+    glib2-devel gd-devel popt-devel rubygem-mustache
 pip install breathe Sphinx
